3455d72b35973a03cab2e635cf4fe32409d969dd
christyleeyx <73215989+christyleeyx@users.noreply.github.com>
2025-01-24 00:59:30 +0800

    Expose JSON data inside response object

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,4 +50,4 @@ if time_to_insert:
 #New Section to display smoothiefroot nutrition information
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
